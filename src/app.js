a0bb78f6e13cfe42f77ab9db76f2830b70708476
Sajadev404 <empresasarn@gmail.com>
2024-08-30 09:12:21 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector('#excuse').innerHTML = excuseGenerate();
+};
+let excuseGenerate = () => {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let whopt = Math.floor(Math.random()* who.length);
+  let actionpt = Math.floor(Math.random()* action.length);
+  let whatpt = Math.floor(Math.random()* what.length);
+  let whenpt = Math.floor(Math.random()* when.length);
+  return who[whopt] + ' ' +action[actionpt] + ' ' +what[whatpt] + ' ' +when[whenpt];
 };
